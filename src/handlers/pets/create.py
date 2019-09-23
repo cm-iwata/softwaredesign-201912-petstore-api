8af5220fcee453459c4ad6df1a563face00d8ddc
Tomoya Iwata <iwata.tomoya@classmethod.jp>
2019-09-23 15:50:54 +0900

    Chapter3 Step5 ペット登録処理の実装

--- src/handlers/pets/create.py
+++ src/handlers/pets/create.py
@@ -1,5 +1,18 @@
+import json
+import os
+
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+pets_table = dynamodb.Table(os.environ['PETS_TABLE'])
+
+
 def handler(event, context):
+
+    data = json.loads(event['body'])
+    pets_table.put_item(Item=data)
+
     return {
       'statusCode': 201,
-      'body': 'created'
+      'body': json.dumps(data)
     }

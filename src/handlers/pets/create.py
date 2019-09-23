e010711cc06af5a24ab9d31c730ea6da5fd52d42
Tomoya Iwata <iwata.tomoya@classmethod.jp>
2019-09-23 15:50:54 +0900

    Chapter3 Step6 バリデーション処理の実装

--- src/handlers/pets/create.py
+++ src/handlers/pets/create.py
@@ -1,18 +1,82 @@
 import json
+from json.decoder import JSONDecodeError
 import os
 
 import boto3
+from cerberus import Validator
 
 dynamodb = boto3.resource('dynamodb')
 pets_table = dynamodb.Table(os.environ['PETS_TABLE'])
+schema = {
+    'id': {
+        'type': 'integer',
+        'required': True,
+        'min': 1,
+    },
+    'name': {
+        'type': 'string',
+        'minlength': 1,
+        'maxlength': 50,
+        'required': True,
+    },
+    'tags': {
+        'type': 'list',
+        'required': False,
+        'schema': {
+            'type': 'dict',
+            'schema': {
+                'id': {
+                    'type': 'integer',
+                    'required': True,
+                },
+                'name': {
+                    'type': 'string',
+                    'minlength': 1,
+                    'maxlength': 50,
+                    'required': True,
+                }
+            }
+        }
+    }
+}
 
 
 def handler(event, context):
 
+    if not __is_valid_json(event['body']):
+        return {
+            'statusCode': 400,
+            'body': json.dumps({
+                'message': "request body doesn't contain valid json"
+            })
+        }
+
     data = json.loads(event['body'])
+    v = Validator(schema)
+    if v.validate(data) is not True:
+        return {
+            'statusCode': 400,
+            'body': json.dumps({
+                'message': v.errors
+            })
+        }
+
     pets_table.put_item(Item=data)
 
     return {
       'statusCode': 201,
       'body': json.dumps(data)
     }
+
+
+def __is_valid_json(src):
+    if src is None:
+        return False
+
+    try:
+        res = json.loads(src)
+        if isinstance(res, dict) or isinstance(res, list):
+            return True
+        return False
+    except JSONDecodeError:
+        return False
--- tests/unit/handlers/pets/test_create_pets.py
+++ tests/unit/handlers/pets/test_create_pets.py
@@ -39,3 +39,59 @@ def test_valid_data_will_creates_pet():
     item = dynamo_res['Item']
     assert item['id'] == 1
     assert item['name'] == 'dog'
+
+
+@pytest.mark.parametrize('data,res_body', [
+    (
+            'invalid_json',
+            '{"message": "request body doesn\'t contain valid json"}'
+    ),
+    (
+            {'id': 1},
+            '{"message": {"name": ["required field"]}}'
+    ),
+    (
+            {'id': 1, 'name': ''},
+            '{"message": {"name": ["min length is 1"]}}'
+    ),
+    (
+            {'id': 0, 'name': 'dog'},
+            '{"message": {"id": ["min value is 1"]}}'
+    ),
+    (
+            {'name': 'dog'},
+            '{"message": {"id": ["required field"]}}'
+    ),
+    (
+            {'id': 'str_val', 'name': 'dog'},
+            '{"message": {"id": ["must be of integer type"]}}'
+    ),
+    (
+            {'id': 1, 'name': '123456789012345678901234567890123456789012345678901'},
+            '{"message": {"name": ["max length is 50"]}}'
+    ),
+    (
+            {'id': 1, 'name': 'dog', 'tags': [{'id': 1}]},
+            '{"message": {"tags": [{"0": [{"name": ["required field"]}]}]}}'
+    ),
+    (
+            {'id': 1, 'name': 'dog', 'tags': [{'id': 1, 'name': ''}]},
+            '{"message": {"tags": [{"0": [{"name": ["min length is 1"]}]}]}}'
+    ),
+    (
+            {'id': 1, 'name': 'dog', 'tags': [
+                {'id': 1, 'name': '123456789012345678901234567890123456789012345678901'}]
+             },
+            '{"message": {"tags": [{"0": [{"name": ["max length is 50"]}]}]}}'
+    ),
+
+])
+def test_invalid_json_will_return_bad_request(data, res_body):
+
+    event = {
+        'body': json.dumps(data)
+    }
+
+    res = handler(event, {})
+    assert res['statusCode'] == 400
+    assert res['body'] == res_body
